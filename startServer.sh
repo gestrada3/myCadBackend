38b89a8195f944790cff0bd3a0a948533fc1b7d4
Giovani Estrada <giovani.estrada@gmail.com>
2025-11-14 01:09:14 +0000

    Restart rails server

--- startServer.sh
+++ startServer.sh
@@ -3,7 +3,10 @@
 PORT=80
 
 # Make sure the port is free and start the application
-lsof -t -wni tcp:${PORT} | xargs kill -9
-nohup rails server --binding 0.0.0.0 --port ${PORT} >/dev/null 2>&1 &
+PID=$(lsof -t -wni tcp:${PORT})
+[ ! -z ${PID} ] && kill -9 ${PID}
+rm -f tmp/pids/server.pid
+
+sudo -b --preserve-env=PATH env nohup rails server --binding 0.0.0.0 --port ${PORT}
 
 exit
